69fff508bb5423d0d8e9bd8125af0bb8a01f12c3
DesalegnTamirat <81221215+DesalegnTamirat@users.noreply.github.com>
2023-01-17 03:15:59 +0300

    Create selectionsort.py

---
+++ selectionsort.py
@@ -0,0 +1,7 @@
+def selectionSort(self, arr,n):
+        for i in range(n):
+            for j in range(i+1,n):
+                if(arr[i]>arr[j]):
+                    print(arr)
+                    arr[i],arr[j]=arr[j],arr[i]
+        return arr
