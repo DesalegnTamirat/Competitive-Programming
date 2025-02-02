b02efa74aea422b55ddc455b06b5af7322408ec0
Desalegn Tamirat <81221215+DesalegnTamirat@users.noreply.github.com>
2025-02-02 21:17:44 +0300

    Created 'Sum' by A2SV Hub

---
+++ 02-Feb-2025/Sum 234830.py
@@ -0,0 +1,5 @@
+# Problem: Sum - https://codeforces.com/contest/1742/problem/A
+
+for _ in range(int(input())):
+  a, b, c = sorted(map(int, input().split()))
+  print("YES" if a + b == c else "NO")
